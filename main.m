acfba8ce7be453c82db5ebb727eb627d483f07b3
Pasquale De Rosa <pasqualed70@icloud.com>
2026-03-25 18:12:04 -0400

    Implementing online phase. Building fmincon problem

--- main.m
+++ main.m
@@ -13,11 +13,17 @@ kf = 44;        % sim. Time
 t = 0:Ts:kf;    % Array of discrete time steps
 N = length(t);
 
-% Pre-allocate variables for speed and ref.
+% Pre-allocate variables 
 d_norms = zeros(1, N);
+
 xr = zeros(1,N);
 yr = zeros(1,N);
 theta_r = zeros(1,N);
+
+vr = zeros(1,N);
+wr = zeros(1,N);
+
+ur = zeros(2,N);
 
 %% Compute reference trajectory
 
@@ -37,13 +43,13 @@ for k = 1:N
     y_ddot = -(0.6 / 7.0^2) * sin(tk / 7.0);
     
     % Unicycle Reference Velocities (Eq 11) 
-    vr = sqrt(x_dot^2 + y_dot^2);
+    vr(k) = sqrt(x_dot^2 + y_dot^2);
     
     % Prevent division by zero 
     if (x_dot^2 + y_dot^2) == 0
-        wr = 0;
+        wr(k) = 0;
     else
-        wr = (y_ddot * x_dot - x_ddot * y_dot) / (x_dot^2 + y_dot^2);
+        wr(k) = (y_ddot * x_dot - x_ddot * y_dot) / (x_dot^2 + y_dot^2);
     end
     
     theta_r(k) = atan2(y_dot, x_dot);
@@ -52,11 +58,11 @@ for k = 1:N
     T_FL_inv = [cos(theta_r(k)), -b * sin(theta_r(k)); 
                 sin(theta_r(k)),  b * cos(theta_r(k))];
             
-    ur = T_FL_inv * [vr; wr];
+    ur(:,k) = T_FL_inv * [vr(k); wr(k)];
     
     % Disturbance d(k) (Eq 16) 
     B = eye(2) * Ts;
-    d = -B * ur;
+    d = -B * ur(:,k);
     
     % Euclidean norm (length) of the disturbance vector
     d_norms(k) = norm(d);
@@ -66,7 +72,7 @@ end
 radii.rD = max(d_norms);
 fprintf('radius of the disturbance set D (rd): %.4f\n', radii.rD);
 
-%% Plots
+% Plots
 figure;
 hold on;
 title('Reference Trajectory')
@@ -117,5 +123,72 @@ function boolean = isCovered(zTilde0, r)
 end
 
 radii.T = radii.T(1:i-1);
+Nradii = length(radii.T);
+
+% Plots
+figure;
+hold on;
+title('ROSC sets $\mathcal{T}_i, i=1,\dots,N$', 'Interpreter','latex')
+grid on;
+theta_k = linspace(0,2*pi,360);
+for i = 1:Nradii
+    r = radii.T(i);
+    x = r*cos(theta_k);
+    y = r*sin(theta_k);
+    if i == 1
+        hRed = plot(x,y,'-','Color','r');
+    else
+        plot(x,y,'-','Color','b');
+    end
+    axis equal;
+end
+hDot = plot(zTilde0(1),zTilde0(2),'LineStyle','none','Marker','hexagram','MarkerSize',8,'Color','black','MarkerFaceColor','black');
+legend( [hRed,hDot],{'Set $\mathcal{D}$','$\tilde{z}(0)$'},'Interpreter','latex');
+hold off
 
 %% Online
+X = zeros(3,N);
+X(:,1) = q0;
+
+Z = zeros(2,N);
+Zr = zeros(2,N);
+ZTilde = zeros(2,N);
+
+Hd = [-1/omegaBar   0        1/omegaBar      0;
+      0         -1/omegaBar      0         1/omegaBar]';
+T = [R/2 R/2; R/D -R/D];
+
+for k = 1:N
+    % Compute zTilde, store
+    theta_k = X(3,k);
+    z_k = X(1:2,k) + [b * cos(theta_k); b * sin(theta_k)];
+    zr_k = [xr(k) + b * cos(theta_r(k)); yr(k) + b * sin(theta_r(k))];
+    zTilde_k = z_k - zr_k;
+    
+    Z(:,k) = z_k;
+    Zr(:,k) = zr_k;
+    ZTilde(:,k) = zTilde_k;
+
+    % Compute U(theta)
+    T_FL_theta_k = [cos(theta_k) sin(theta_k); -sin(theta_k)/b cos(theta_k)/b];
+    H_theta_k = Hd * inv(T) * T_FL_theta_k;
+
+    % Compute ur_k
+    
+    % Find i_min
+    i_min = 0;
+    for i = 1:Nradii
+        if isCovered(zTilde_k, radii.T(i))
+            i_min = i;
+        end
+    end
+
+    % Solve optimization
+    if i == 1
+        
+    else
+
+    end
+
+
+end
---
+++ optimization/constr.m
@@ -0,0 +1,5 @@
+function [c,ceq] = constr(u, A, B, z_tilde_k, ur_k, r1)
+    z_tilde_k_1 = A * z_tilde_k + B * u - B * ur_k;
+    c = norm(z_tilde_k_1) - r1;  
+    ceq = [];
+end
